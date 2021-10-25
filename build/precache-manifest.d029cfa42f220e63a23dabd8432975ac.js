eb47e50d98e76ec2dd63c67229ea375e319bc3d4
MondiKoci <mondi.koci@georgebrown.ca>
2021-10-25 17:35:40 -0400

    last implementation test

--- build/precache-manifest.d029cfa42f220e63a23dabd8432975ac.js
+++ build/precache-manifest.d029cfa42f220e63a23dabd8432975ac.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "3644076130dbd04cac44",
-    "url": "/MondiKoci/Comp3104/static/css/main.406a9054.chunk.css"
+    "revision": "2cc45a602967f964eecc",
+    "url": "/MondiKoci/Comp3104/static/css/main.d0f7c7b3.chunk.css"
   },
   {
-    "revision": "3644076130dbd04cac44",
-    "url": "/MondiKoci/Comp3104/static/js/main.36440761.chunk.js"
+    "revision": "2cc45a602967f964eecc",
+    "url": "/MondiKoci/Comp3104/static/js/main.2cc45a60.chunk.js"
   },
   {
     "revision": "ed7dc9cc3f33feb93cd4",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/MondiKoci/Comp3104/static/js/2.8ebec04f.chunk.js"
   },
   {
-    "revision": "b94268e35ad5dcf4cb84c9e8e111a711",
+    "revision": "c451b918e3490ea62086b6a9981adda1",
     "url": "/MondiKoci/Comp3104/index.html"
   }
 ];

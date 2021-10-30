c2e0e58b38a4e602691ab86b537bd29e56fd4a2b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-30 18:45:22 +0000

    Deploy MondiKoci/Comp3104 to github.com/MondiKoci/Comp3104.git:gh-pages

---
+++ precache-manifest.ab0588e342d55e908a2c7947d7687c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c71a4a5e1f7ef71a7a3",
+    "url": "/MondiKoci/Comp3104/static/css/main.9bfb0264.chunk.css"
+  },
+  {
+    "revision": "0c71a4a5e1f7ef71a7a3",
+    "url": "/MondiKoci/Comp3104/static/js/main.0c71a4a5.chunk.js"
+  },
+  {
+    "revision": "ed7dc9cc3f33feb93cd4",
+    "url": "/MondiKoci/Comp3104/static/js/runtime~main.ed7dc9cc.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/MondiKoci/Comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "260adc9352fbe9cb60d0e546735ac834",
+    "url": "/MondiKoci/Comp3104/index.html"
+  }
+];
